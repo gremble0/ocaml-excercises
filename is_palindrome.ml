0cd914f7f3cd8501ad7ef1670ecc7e1dc79fc3fa
gremble0 <hermanstornes@gmail.com>
2023-06-18 16:35:05 +0200

    Fixed problem5

--- is_palindrome.ml
+++ is_palindrome.ml
@@ -1,4 +1,3 @@
-(* #use "./rev.ml"; *)
 let rev (list: 'a list): 'a list = 
     let rec rev' (list: 'a list) (builder: 'a list): 'a list =
         match list with
@@ -6,19 +5,14 @@ let rev (list: 'a list): 'a list =
         | x :: rest -> rev' rest (x :: builder)
     in rev' list []
 
-(*wrong*)
 let is_palindrome (list: 'a list): bool = 
     let reversed_list = rev list in
-    match reversed_list with
-    | list -> 
-        if list = reversed_list
+    if list = reversed_list
         then true
         else false
-
 
 let () = 
     assert (is_palindrome [1; 2; 3] = false);
     assert (is_palindrome ["a"; "b"; "a"] = true);
     assert (is_palindrome [1] = true);
     assert (is_palindrome [] = true);
-
